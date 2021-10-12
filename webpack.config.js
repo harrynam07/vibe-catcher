6eb18753fb21b8d024cace87278605bf4fd68fb4
Harrison Nam <harrison.j.nam@gmail.com>
2021-10-12 12:22:44 -0700

    Buildling webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'public')
+    },
+    mode: 'production',
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+};
