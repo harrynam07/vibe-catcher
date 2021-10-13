d7b9e8c68aa1d9457632bf234a4e90e3db7e4a9e
Harrison Nam <harrison.j.nam@gmail.com>
2021-10-13 13:12:09 -0700

    Successfully redirected to Spotify authorization page

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,12 +5,26 @@ const router = express.Router();
 const CLIENT_ID = 'c1f1711630d54c4cac579bea402f2253';
 const CLIENT_SECRET = '11683595db924c368d35a322ab415a25';
 
+//spotify request scopes
+const scopes = 'user-library-modify';
+// 'user-library-modify playlist-modify-private playlist-read-priavate user-read-private user-read-playback-position user-read-recently-played user-modify-playback-state user-follow-modify playlist-modify-public user-follow-read user-read-currently-playing user-library-read streaming user-top-read';
+//redirect uri
+const redirect_uri = 'http://localhost:8080';
+
 //spotify endpoints:
 const authorize = 'https://accounts.spotify.com/authorize';
 
+const generateUrl = (url) => {
+  url += '?client_id=' + CLIENT_ID;
+  url += '&response_type=code';
+  url += '&redirect_uri=' + redirect_uri;
+  url += '&show_dialogue=true';
+  url += '&scope=' + scopes;
+  return url;
+};
 //get request for initial authorization 
 router.get('/', (req, res) => {
-  return res.redirect(authorize);
+  return res.redirect(generateUrl(authorize));
 });
 
 module.exports = router;
