b40e66a111fab49f1c0b709099dbe19d7b34750e
Harrison Nam <harrison.j.nam@gmail.com>
2021-10-13 13:49:39 -0700

    Updated scopes for Spotify authorization

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,7 +6,7 @@ const CLIENT_ID = 'c1f1711630d54c4cac579bea402f2253';
 const CLIENT_SECRET = '11683595db924c368d35a322ab415a25';
 
 //spotify request scopes
-const scopes = 'user-library-modify';
+const scopes = 'user-library-modify streaming user-top-read user-library-read user-read-currently-playing user-follow-read playlist-modify-public user-read-email user-modify-playback-state user-read-recently-played user-read-playback-position user-read-playback-state user-read-private playlist-read-private playlist-modify-private';
 // 'user-library-modify playlist-modify-private playlist-read-priavate user-read-private user-read-playback-position user-read-recently-played user-modify-playback-state user-follow-modify playlist-modify-public user-follow-read user-read-currently-playing user-library-read streaming user-top-read';
 //redirect uri
 const redirect_uri = 'http://localhost:8080';
