500b0a5bbe14c71549d0134e1c53ead2e2498eb5
Harrison Nam <harrison.j.nam@gmail.com>
2021-10-12 18:35:43 -0700

    localhost:8000 proxying /api requests to localhost:3000

--- server/server.js
+++ server/server.js
@@ -10,6 +10,11 @@ app.use(express.urlencoded({extended: true}));
 app.get('/', (req, res) => {
     res.status(200).send("Hello World");
 });
+
+app.get('/api', (req, res) => {
+    console.log('request received');
+    res.status(201).send("Lets gooo");
+})
 //server listening on port 3000
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
